be5fc0782f10486af7db7d22e2520a0c2bedd118
Frucou <fruicou.sansus3@gmail.com>
2021-11-03 13:02:09 +0100

    Ejercicio acabado
    Añadidos:
    -Todos los elementos del index necesario
    -Todos los elementos del css necesario
    -Todos los elementos del js necesario
    No implementados:
    -el content del ::before no carga y desorganiza lo que tenia en el css

---
+++ js/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+//Nodos
+const btn1 = document.querySelector(".btn1")
+let error = document.querySelector(".error")
+const body = document.querySelector("body")
+// const btn2 = document.querySelector(".btn2")
+
+
+//Variables
+let edad;
+
+//Funciones
+const pregunta = () => {
+    edad = prompt("Dime tu edad");
+    // error.textContent = 0;
+    // console.log(edad)
+    // edad = Number(edad);
+    console.log(edad)
+    if (isNaN(edad)==false) {
+        if (edad>=18) {
+            // las siguientes líneas son formas de implementar
+            // css al js para pintar algo tras una acción
+            body.style.backgroundColor = "skyblue";
+            // console.log("blue")
+        } else {
+            document.body.style.backgroundColor = "pink";
+            // console.log("pink")
+        }
+    } else {
+        error.textContent = `Debe meter un número --> ${edad}`;
+    }
+    
+};
+
+
+// const cambio = () => {
+//     alert("this");
+// }
+
+//Eventos
+btn1.addEventListener(
+    "click",
+    pregunta
+);
+
+// btn2.addEventListener(
+//     "click",
+//     cambio
+// );
